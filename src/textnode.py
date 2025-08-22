041098b0a3368a0f275e833b52660704412f2933
Emal <EmalHasitha@proton.me>
2025-08-22 06:40:10 +0530

    feat: add TextNode class

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url
+
+    def __eq__(self, text_node):
+        return (
+            self.text == text_node.text
+            and self.text_type.value == text_node.text_type.value
+            and self.url == self.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
