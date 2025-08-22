7c066fb273d5fac262e3a631c06abf101b3354e0
Emal <EmalHasitha@proton.me>
2025-08-22 07:18:42 +0530

    test: add unit tests for ParentNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -32,3 +32,17 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_p2(self):
         node = LeafNode("p", "Hello, world!", {"class": "text-sm"})
         self.assertEqual(node.to_html(), '<p class="text-sm">Hello, world!</p>')
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
